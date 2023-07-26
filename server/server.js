6ec3dbfd5cd02a616e0d1a983ff98b523012c6b7
Utsav Joshi <joshiutsav56@gmail.com>
2023-07-26 15:19:13 +0530

    Day-1 26|07|2023

--- server/server.js
+++ server/server.js
@@ -1,17 +1,27 @@
 const env = require("dotenv");
-const dotenv = env.config();
 const path = require("path");
+const http = require("http");
 const express = require("express");
+const socketIo = require("socket.io");
 
-const port = process.env.PORT;
 
 const publicPath = path.join(__dirname, "/../public");
-
+const port = process.env.PORT || 80;
 const app = express();
+const dotenv = env.config();
+let server = http.createServer(app);
+let io = socketIo(server);
 
 app.use(express.static(publicPath));
 
+io.on('connection', (socket) => {
+    console.log("new user joined");
+    socket.on('disconnect', () => {
+        console.log("user was disconnected from server");
+    });
+});
 
-app.listen( port, () => {
+
+server.listen(port, () => {
     console.log(`http://localhost/`);
 })
