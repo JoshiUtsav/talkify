7690f017acdc17b2cbb16837f1fef8ab1af3d1a7
Utsav Joshi <joshiutsav56@gmail.com>
2023-07-27 11:41:05 +0530

    27|07|2023 Day2

--- server/server.js
+++ server/server.js
@@ -16,6 +16,28 @@ app.use(express.static(publicPath));
 
 io.on('connection', (socket) => {
     console.log("new user joined");
+
+    socket.emit('newMessage', {
+        from: 'Admin',
+        text: 'Welcome to my room',
+        createdAt: new Date().getTime()
+    })
+
+    socket.broadcast.emit('newMessage', {
+        from: 'Admin',
+        text: 'New User Joined',
+        createdAt: new Date().getTime()
+    })
+
+    socket.on("createMessage", message => {
+        console.log("createMessage", message);
+        io.emit('newMessage', {
+            from:  message.from,
+            text: message.text,
+            createdAt: new Date().getTime()
+        })
+    })
+
     socket.on('disconnect', () => {
         console.log("user was disconnected from server");
     });
